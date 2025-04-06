06d38513afb1c043dc642774f5c300d8a2cfd330
KayKay1992 <kennethnwankpa1992@gmail.com>
2025-04-05 22:30:18 -0700

    Defining App Routes For frontend applications

--- frontend/Task-Manager/src/App.jsx
+++ frontend/Task-Manager/src/App.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Auth/Login";
+import SignUp from "./pages/Auth/SignUp";
+import Dashboard from "./pages/Admin/Dashboard";
+import ManageTasks from "./pages/Admin/ManageTasks";
+import CraeteTask from "./pages/Admin/CraeteTask";
+import ManageUsers from "./pages/Admin/ManageUsers";
+import UserDashboard from "./pages/User/UserDashboard";
+import MyTasks from "./pages/User/MyTasks";
+import ViewTaskDetails from "./pages/User/ViewTaskDetails";
+import PrivateRoute from "./routes/PrivateRoute";
 
 function App() {
   return (
-    <div className='text-3xl text-red-500'>App</div>
-  )
+    <div>
+      <Router>
+        <Routes>
+          {/* auth routes here */}
+          {/* <Route path="/" element={<Home />} /> */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/signUp" element={<SignUp />} />
+
+          {/* Add admin routes here */}
+          <Route element={<PrivateRoute allowedRoles={["admin"]} />}>
+            <Route path="/admin/dashboard" element={<Dashboard />} />
+            <Route path="/admin/tasks" element={<ManageTasks />} />
+            <Route path="/admin/create-task" element={<CraeteTask/>} />
+            <Route path="/admin/users" element={<ManageUsers/>} />
+          </Route>
+
+           {/* Add users routes here */}
+           <Route element={<PrivateRoute allowedRoles={["admin"]} />}>
+            <Route path="/user/dashboard" element={<UserDashboard />} />
+            <Route path="/user/tasks" element={<MyTasks />} />
+            <Route path="/user/task-details/:id" element={<ViewTaskDetails />} />
+          </Route>
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
-export default App
+export default App;
