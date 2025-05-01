fb979f12890af5327f04c92f8ac39b9c7bcd2862
KayKay1992 <kennethnwankpa1992@gmail.com>
2025-05-01 12:04:58 -0700

    Creating Summary section

--- frontend/Task-Manager/src/pages/Admin/Dashboard.jsx
+++ frontend/Task-Manager/src/pages/Admin/Dashboard.jsx
@@ -5,6 +5,9 @@ import DashboardLayout from '../../components/layouts/DashboardLayout';
 import { useNavigate } from 'react-router-dom';
 import axiosInstance from '../../utils/axiosInstance';
 import { API_PATHS } from '../../utils/apiPaths';
+import moment from 'moment'
+import InfoCard from '../../components/Cards/InfoCard';
+import { addThousandsSeparator } from '../../utils/helper';
 
 const Dashboard = () => {
   useUserAuth();
@@ -36,7 +39,42 @@ const Dashboard = () => {
   }, [])
   
   return (
-    <DashboardLayout activeMenu="Dashboard">Dashboard</DashboardLayout>
+    <DashboardLayout activeMenu="Dashboard">
+      <div className="card my-3">
+        <div className="">
+          <div className="col-span-3">
+            <h2 className="text-xl md:text-2xl">
+              Good Day! {user?.name}
+            </h2>
+            <p className="text-xs md:text-[13px] text-gray-400 mt-1.5">
+              {moment().format('dddd Do MMM YYYY')}
+            </p>
+          </div>
+        </div>
+        <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 md:gap-6 ">
+          <InfoCard
+             label='Total Tasks'
+             value={addThousandsSeparator(dashboardData?.charts?.taskDistribution?.All || 0)}
+             color='bg-primary'
+             />
+          <InfoCard
+             label='Pending Tasks'
+             value={addThousandsSeparator(dashboardData?.charts?.taskDistribution?.Pending || 0)}
+             color='bg-violet-500'
+             />
+          <InfoCard
+             label='In Progress Tasks'
+             value={addThousandsSeparator(dashboardData?.charts?.taskDistribution?.InProgress || 0)}
+             color='bg-cyan-500'
+             />
+          <InfoCard
+             label='Completed Tasks'
+             value={addThousandsSeparator(dashboardData?.charts?.taskDistribution?.Completed || 0)}
+             color='bg-lime-500'
+             />
+        </div>
+      </div>
+    </DashboardLayout>
   )
 }
 
