d6d6fbbf2b7f01c3413de2e94b4773a3df5fd8ba
KayKay1992 <kennethnwankpa1992@gmail.com>
2025-05-04 08:39:50 -0700

    CREATING  Task Priority Levels DISTRIBUTION SECTION (BAR CHART)

--- frontend/Task-Manager/src/pages/Admin/Dashboard.jsx
+++ frontend/Task-Manager/src/pages/Admin/Dashboard.jsx
@@ -11,6 +11,7 @@ import { addThousandsSeparator } from '../../utils/helper';
 import { LuArrowRight } from 'react-icons/lu';
 import TaskListTable from '../../components/TaskListTable';
 import CustomPieChart from '../../components/Charts/CustomPieChart';
+import CustomBarChart from '../../components/Charts/CustomBarChart';
 
 const COLORS = ["#8D51FF", "#00B8DB", "#7BCE00"]
 
@@ -116,6 +117,18 @@ const Dashboard = () => {
             </div>
 
            <CustomPieChart data={pieChartData} colors={COLORS}/>
+          </div>
+         </div>
+
+         <div className="">
+          <div className="card">
+            <div className="flex items-center justify-between">
+              <h5 className="font-medium">
+                Task Priority Levels
+              </h5>
+              </div>
+              <CustomBarChart data={barChartData} />
+           
           </div>
          </div>
         <div className="md:col-span-2">
