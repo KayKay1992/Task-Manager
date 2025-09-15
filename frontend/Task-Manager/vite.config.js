58fed8d522f5400b0a6ae87eb93595386c476598
KayKay1992 <kennethnwankpa1992@gmail.com>
2025-09-15 07:25:28 -0700

    building

--- frontend/Task-Manager/vite.config.js
+++ frontend/Task-Manager/vite.config.js
@@ -1,8 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),   tailwindcss(),],
+  plugins: [
+    react(),
+    tailwindcss(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/_redirects', // make sure this file exists in your public folder
+          dest: '.'                 // copy into dist/
+        }
+      ]
+    })
+  ],
+  build: {
+    outDir: 'dist',
+    chunkSizeWarningLimit: 1000,
+  },
 })
